46ab7ba734916541f31ed213850f09ae8972c01a
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-12-07 13:57:42 +0300

    feat(#1): add the rultor configuration file

--- src/main/java/org/eolang/opeo/App.java
+++ src/main/java/org/eolang/opeo/App.java
@@ -2,6 +2,8 @@ package org.eolang.opeo;
 
 /**
  * Hello world!
+ * Remove this test as soon as you start developing your own code.
+ * @since 0.1
  */
 public class App {
     public static void main(String[] args) {
--- src/test/java/org/eolang/opeo/AppTest.java
+++ src/test/java/org/eolang/opeo/AppTest.java
@@ -1,17 +1,25 @@
 package org.eolang.opeo;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
+ * Remove this test as soon as you start developing your own code.
+ * @since 0.1
  */
-public class AppTest 
-{
+class AppTest {
+
     /**
-     * Rigorous Test :-)
+     * Rigorous Test.
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void shouldAnswerWithTrue() {
+        MatcherAssert.assertThat(
+            "Dummy test. Remove it!",
+            new App(),
+            Matchers.notNullValue()
+        );
     }
 }
