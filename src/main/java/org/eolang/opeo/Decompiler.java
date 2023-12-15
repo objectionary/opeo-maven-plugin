4f7d2f415b9ae426809fa1d8ed00ebd6f49b6087
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-12-15 19:00:26 +0300

    feat(#8): implement the most of the instruction handlers for simple transformation

--- src/main/java/org/eolang/opeo/Decompiler.java
+++ src/main/java/org/eolang/opeo/Decompiler.java
@@ -1,8 +1,112 @@
 package org.eolang.opeo;
 
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.objectweb.asm.Opcodes;
+
 public class Decompiler {
 
+    private final Deque<Object> stack;
+    private final Deque<String> result;
+
+    private final Map<String, String> heap;
+
+    private final Map<Integer, InstructionHandler> handlers;
+
+    private final AtomicInteger counter;
+
+    public Decompiler() {
+        this(new LinkedList<>(), new LinkedList<>(), Map.of(), new AtomicInteger(0));
+    }
+
+    public Decompiler(
+        final Deque<Object> stack,
+        final Deque<String> result,
+        final Map<String, String> heap,
+        final AtomicInteger counter
+    ) {
+        this.stack = stack;
+        this.result = result;
+        this.heap = heap;
+        this.counter = counter;
+        this.handlers = Map.of(
+            Opcodes.NEW, new NewHandler(),
+            Opcodes.DUP, new DupHandler(),
+            Opcodes.BIPUSH, new BipushHandler(),
+            Opcodes.POP, new PopHandler(),
+            Opcodes.RETURN, new ReturnHandler()
+        );
+    }
+
     public String decompile(Instruction... instructions) {
-        return "";
+        Arrays.stream(instructions)
+            .forEach(inst -> this.handler(inst.opcode()).handle(inst));
+        return String.join(";\n", this.result);
+    }
+
+    private InstructionHandler handler(final int opcode) {
+        return this.handlers.getOrDefault(opcode, new UnimplementedHandler());
+    }
+
+    private interface InstructionHandler {
+        void handle(Instruction instruction);
+    }
+
+    /**
+     * Returns a reference to an object.
+     * @param type Object type.
+     * @return Reference.
+     */
+    private String reference(final String type) {
+        return String.format("%s%d%s",
+            "&",
+            this.counter.getAndIncrement(),
+            type.replace('/', '.')
+        );
+    }
+
+    private class NewHandler implements InstructionHandler {
+        @Override
+        public void handle(Instruction instruction) {
+            Decompiler.this.stack.push(Decompiler.this.reference((String) instruction.operand(0)));
+        }
+    }
+
+    private class DupHandler implements InstructionHandler {
+        @Override
+        public void handle(Instruction instruction) {
+            Decompiler.this.stack.push(Decompiler.this.stack.peek());
+        }
+    }
+
+    private class BipushHandler implements InstructionHandler {
+        @Override
+        public void handle(Instruction instruction) {
+            Decompiler.this.stack.push(instruction.operand(0));
+        }
+    }
+
+    private class PopHandler implements InstructionHandler {
+        @Override
+        public void handle(Instruction instruction) {
+            Decompiler.this.stack.pop();
+        }
+    }
+
+    private class ReturnHandler implements InstructionHandler {
+        @Override
+        public void handle(Instruction instruction) {
+            Decompiler.this.result.add("return");
+        }
+    }
+
+    private class UnimplementedHandler implements InstructionHandler {
+        @Override
+        public void handle(Instruction instruction) {
+            Decompiler.this.result.add(String.format("Unimplemented %s", instruction));
+        }
     }
 }
--- src/main/java/org/eolang/opeo/Instruction.java
+++ src/main/java/org/eolang/opeo/Instruction.java
@@ -1,7 +1,9 @@
 package org.eolang.opeo;
 
 import java.util.List;
+import lombok.ToString;
 
+@ToString
 public class Instruction {
     private final int opcode;
     private final List<Object> operands;
@@ -13,5 +15,13 @@ public class Instruction {
     public Instruction(final int opcode, final List<Object> operands) {
         this.opcode = opcode;
         this.operands = operands;
+    }
+
+    public int opcode() {
+        return this.opcode;
+    }
+
+    public Object operand(int index) {
+        return this.operands.get(index);
     }
 }
