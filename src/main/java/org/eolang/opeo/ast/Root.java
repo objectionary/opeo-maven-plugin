8fcdc8962d13ef10cdc4af16de91775ef21cec50
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-12-20 15:39:13 +0300

    feat(#12): simple implementation of Root transformation into XMIR

--- src/main/java/org/eolang/opeo/ast/Root.java
+++ src/main/java/org/eolang/opeo/ast/Root.java
@@ -23,12 +23,14 @@
  */
 package org.eolang.opeo.ast;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import org.xembly.Directive;
+import org.xembly.Directives;
 
 /**
  * Root node.
@@ -55,7 +57,17 @@ public final class Root implements AstNode {
 
     @Override
     public Iterable<Directive> toXmir() {
-        return null;
+        final Iterable<Directive> result;
+        if (this.children.isEmpty()) {
+            result = Collections.emptyList();
+        } else {
+            final Directives directives = new Directives();
+            directives.add("o").attr("base", "tuple");
+            this.children.stream().map(AstNode::toXmir).forEach(directives::append);
+            directives.up();
+            result = directives;
+        }
+        return result;
     }
 
     /**
