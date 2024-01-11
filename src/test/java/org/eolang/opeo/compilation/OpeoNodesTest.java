c84c0db9d6c47133fee2d1e8302b647ad917e8c7
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-01-11 18:17:36 +0300

    feat(#65): simplify code in OpeoNodesTest

--- src/test/java/org/eolang/opeo/compilation/OpeoNodesTest.java
+++ src/test/java/org/eolang/opeo/compilation/OpeoNodesTest.java
@@ -23,13 +23,21 @@
  */
 package org.eolang.opeo.compilation;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.eolang.jeo.representation.xmir.XmlNode;
 import org.eolang.opeo.ast.Add;
 import org.eolang.opeo.ast.Literal;
 import org.eolang.opeo.ast.Opcode;
+import org.hamcrest.Description;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.jupiter.api.Test;
 import org.objectweb.asm.Opcodes;
 
@@ -45,17 +53,16 @@ final class OpeoNodesTest {
             new Opcode(Opcodes.ICONST_0, false), new Opcode(Opcodes.POP, false)
         ).toJeoNodes();
         MatcherAssert.assertThat(
-            "We expect the first node to be ICONST_0",
-            nodes.get(0).toString(),
-            Matchers.equalTo(
-                "<o base=\"opcode\" name=\"ICONST_0\">\n   <o base=\"int\" data=\"bytes\">00 00 00 00 00 00 00 03</o>\n</o>\n"
-            )
+            "We expect to retrieve 2 opcodes, but got something else instead: %n%s%n",
+            nodes,
+            Matchers.hasSize(2)
         );
         MatcherAssert.assertThat(
-            "We expect the second node to be POP",
-            nodes.get(1).toString(),
-            Matchers.equalTo(
-                "<o base=\"opcode\" name=\"POP\">\n   <o base=\"int\" data=\"bytes\">00 00 00 00 00 00 00 57</o>\n</o>\n"
+            "We expect to have specific opcodes in right order as is",
+            nodes,
+            new HasInstructions(
+                Opcodes.ICONST_0,
+                Opcodes.POP
             )
         );
     }
@@ -74,19 +81,148 @@ final class OpeoNodesTest {
             Matchers.hasSize(3)
         );
         MatcherAssert.assertThat(
-            "We expect the first node to be ICONST_1",
-            nodes.get(0).attribute("name").get(),
-            Matchers.containsString("ICONST_1")
+            "We expect to have specific opcodes in right order",
+            nodes,
+            new HasInstructions(
+                Opcodes.ICONST_1,
+                Opcodes.ICONST_2,
+                Opcodes.IADD
+            )
         );
+    }
+
+    @Test
+    void convertsDeepAddition() {
+        final List<XmlNode> nodes = new OpeoNodes(
+            new Add(
+                new Add(
+                    new Literal(1),
+                    new Literal(2)
+                ),
+                new Add(
+                    new Literal(3),
+                    new Literal(4)
+                )
+            )
+        ).toJeoNodes();
         MatcherAssert.assertThat(
-            "We expect the second node to be ICONST_2",
-            nodes.get(1).attribute("name").get(),
-            Matchers.containsString("ICONST_2")
+            nodes,
+            new HasInstructions()
         );
-        MatcherAssert.assertThat(
-            "We expect the third node to be IADD",
-            nodes.get(2).attribute("name").get(),
-            Matchers.containsString("IADD")
-        );
+    }
+
+    /**
+     * Matcher for {@link List} of {@link XmlNode} to have specific instructions.
+     * @since 0.1
+     */
+    private static class HasInstructions extends TypeSafeMatcher<List<XmlNode>> {
+
+        /**
+         * Expected opcodes.
+         */
+        private final List<Integer> opcodes;
+
+        /**
+         * Bag of collected warnings.
+         */
+        private final List<String> warnings;
+
+        /**
+         * Constructor.
+         * @param opcodes Expected opcodes.
+         */
+        private HasInstructions(int... opcodes) {
+            this(IntStream.of(opcodes).boxed().collect(Collectors.toList()));
+        }
+
+        /**
+         * Constructor.
+         * @param opcodes Expected opcodes.
+         */
+        private HasInstructions(final List<Integer> opcodes) {
+            this.opcodes = opcodes;
+            this.warnings = new ArrayList<>(0);
+        }
+
+        @Override
+        protected boolean matchesSafely(final List<XmlNode> item) {
+            final int size = item.size();
+            for (int index = 0; index < size; ++index) {
+                final XmlNode node = item.get(index);
+                final Optional<String> obase = node.attribute("base");
+                if (obase.isPresent()) {
+                    final String base = obase.get();
+                    if (base.equals("opcode")) {
+                        final Optional<String> oname = node.attribute("name");
+                        if (oname.isPresent()) {
+                            final String expected = HasInstructions.name(this.opcodes.get(index));
+                            if (!oname.get().contains(expected)) {
+                                this.warnings.add(
+                                    String.format(
+                                        "Expected to have opcode with name %s at index %d, but got %s instead",
+                                        expected,
+                                        index,
+                                        oname.get()
+                                    )
+                                );
+                                return false;
+                            }
+                        } else {
+                            this.warnings.add(
+                                String.format(
+                                    "Expected to have opcode name at index %d, but got nothing instead",
+                                    index
+                                )
+                            );
+                            return false;
+                        }
+                    } else {
+                        this.warnings.add(
+                            String.format(
+                                "Expected to have opcode at index %d, but got %s instead",
+                                index,
+                                base
+                            )
+                        );
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private static String name(final int opcode) {
+            final Field[] fields = Arrays.stream(Opcodes.class.getFields())
+                .filter(field -> !field.getName().startsWith("T_"))
+                .filter(field -> !field.getName().startsWith("H_"))
+                .filter(field -> !field.getName().startsWith("F_"))
+                .filter(field -> !field.getName().startsWith("ACC"))
+                .filter(field -> !field.getName().startsWith("ASM"))
+                .toArray(Field[]::new);
+            for (final Field field : fields) {
+                if (field.getType() == int.class) {
+                    try {
+                        if (opcode == field.getInt(Opcodes.class)) {
+                            return field.getName();
+                        }
+                    } catch (final IllegalAccessException exception) {
+                        throw new RuntimeException(exception);
+                    }
+                }
+            }
+            throw new IllegalStateException(String.format("Unknown opcode: %d", opcode));
+        }
+
+        @Override
+        public void describeTo(final Description description) {
+            description.appendText(
+                String.format(
+                    "Expected to have %d opcodes, but got %d instead. %n%s%n",
+                    this.opcodes.size(),
+                    this.warnings.size(),
+                    String.join("\n", this.warnings)
+                )
+            );
+        }
     }
 }
