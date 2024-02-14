152b0f7578d02adee12e21603e7de4657f5a8165
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-02-14 18:50:54 +0300

    feat(#97): add javadocs for FieldAssignment

--- src/main/java/org/eolang/opeo/ast/FieldAssignment.java
+++ src/main/java/org/eolang/opeo/ast/FieldAssignment.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2023 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.eolang.opeo.ast;
 
 import java.util.ArrayList;
@@ -6,20 +29,43 @@ import org.objectweb.asm.Opcodes;
 import org.xembly.Directive;
 import org.xembly.Directives;
 
+/**
+ * Field assignment.
+ * <p>{@code
+ *   x.y = 2;
+ * }</p>
+ * @since 0.2
+ */
 public final class FieldAssignment implements AstNode {
 
-    private final InstanceField left;
-    private final AstNode right;
+    /**
+     * The field to assign to.
+     */
+    private final InstanceField field;
 
+    /**
+     * The value to assign.
+     */
+    private final AstNode value;
+
+    /**
+     * Field attributes.
+     */
     private final Attributes attributes;
 
+    /**
+     * Constructor.
+     * @param left The field to assign to
+     * @param right The value to assign
+     * @param attributes Field attributes
+     */
     public FieldAssignment(
         final InstanceField left,
         final AstNode right,
         final Attributes attributes
     ) {
-        this.left = left;
-        this.right = right;
+        this.field = left;
+        this.value = right;
         this.attributes = attributes.type("field");
     }
 
@@ -28,16 +74,16 @@ public final class FieldAssignment implements AstNode {
         return new Directives().add("o")
             .attr("base", ".writefield")
             .attr("scope", this.attributes)
-            .append(this.left.toXmir())
-            .append(this.right.toXmir())
+            .append(this.field.toXmir())
+            .append(this.value.toXmir())
             .up();
     }
 
     @Override
     public List<AstNode> opcodes() {
         final List<AstNode> res = new ArrayList<>(3);
-        res.addAll(this.left.instance().opcodes());
-        res.addAll(this.right.opcodes());
+        res.addAll(this.field.instance().opcodes());
+        res.addAll(this.value.opcodes());
         res.add(
             new Opcode(
                 Opcodes.PUTFIELD,
---
+++ src/test/java/org/eolang/opeo/ast/FieldAssignmentTest.java
@@ -0,0 +1,84 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2023 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.eolang.opeo.ast;
+
+import com.jcabi.matchers.XhtmlMatchers;
+import org.eolang.opeo.compilation.HasInstructions;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+import org.objectweb.asm.Opcodes;
+import org.xembly.ImpossibleModificationException;
+import org.xembly.Xembler;
+
+class FieldAssignmentTest {
+
+    @Test
+    void convertsToXmir() throws ImpossibleModificationException {
+        final String xmir = new Xembler(
+            new WriteField(
+                new This(),
+                new Literal(3),
+                new Attributes("name", "bar")
+            ).toXmir()
+        ).xml();
+        MatcherAssert.assertThat(
+            String.format(
+                "Can't convert 'this.bar = 3' statement to the correct xmir, result is wrong: %n%s%n",
+                xmir
+            ),
+            xmir,
+            XhtmlMatchers.hasXPaths(
+                "./o[@base='.write']",
+                "./o[@base='.write']/o[@base='.bar']",
+                "./o[@base='.write']/o[@base='int' and contains(text(),'3')]"
+            )
+        );
+    }
+
+    @Test
+    void transformsToOpcodes() {
+        final String name = "d";
+        final String owner = "test/Test";
+        final String descriptor = "I";
+        MatcherAssert.assertThat(
+            "Can't transform 'this.a = 1' statement to the correct opcodes, result is wrong",
+            new OpcodeNodes(
+                new WriteField(
+                    new This(),
+                    new Literal(1),
+                    new Attributes()
+                        .name(name)
+                        .owner(owner)
+                        .descriptor(descriptor)
+                )
+            ).opcodes(),
+            new HasInstructions(
+                new HasInstructions.Instruction(Opcodes.ALOAD, 0),
+                new HasInstructions.Instruction(Opcodes.ICONST_1),
+                new HasInstructions.Instruction(Opcodes.PUTFIELD, owner, name, descriptor)
+            )
+        );
+    }
+
+}
