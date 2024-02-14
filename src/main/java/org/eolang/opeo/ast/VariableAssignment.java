83f107ef767bbef797fe3fedbe74c324c7ecf8d2
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-02-14 17:50:57 +0300

    feat(#97): add javadoc for VariableAssignment

--- src/main/java/org/eolang/opeo/ast/VariableAssignment.java
+++ src/main/java/org/eolang/opeo/ast/VariableAssignment.java
@@ -1,24 +1,74 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2023 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.eolang.opeo.ast;
 
 import java.util.ArrayList;
 import java.util.List;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.Type;
 import org.xembly.Directive;
 import org.xembly.Directives;
 
+/**
+ * Variable assignment.
+ * <p>{@code
+ *          x = 2;
+ * }</p>
+ * @since 0.2
+ */
 public final class VariableAssignment implements AstNode {
 
+    /**
+     * Left variable.
+     */
     private final LocalVariable left;
+
+    /**
+     * Right expression.
+     */
     private final AstNode right;
 
+    /**
+     * Attributes.
+     */
     private final Attributes attributes;
 
+    /**
+     * Constructor.
+     * @param left Left variable.
+     * @param right Right expression.
+     */
     public VariableAssignment(final LocalVariable left, final AstNode right) {
         this(left, right, new Attributes());
     }
 
-    public VariableAssignment(
+    /**
+     * Constructor.
+     * @param left Left variable.
+     * @param right Right expression.
+     * @param attributes Attributes.
+     */
+    private VariableAssignment(
         final LocalVariable left,
         final AstNode right,
         final Attributes attributes
@@ -42,27 +92,7 @@ public final class VariableAssignment implements AstNode {
     public List<AstNode> opcodes() {
         final List<AstNode> res = new ArrayList<>(3);
         res.addAll(this.right.opcodes());
-        res.add(new Opcode(this.opcode(), this.left.id()));
+        res.add(new Opcode(this.left.type().getOpcode(Opcodes.ISTORE), this.left.id()));
         return res;
-    }
-
-    private int opcode() {
-        final Type type = this.left.type();
-        return type.getOpcode(Opcodes.ISTORE);
-//        if (type.equals(Type.INT_TYPE)) {
-//            return Opcodes.ISTORE;
-//        } else if (type.equals(Type.DOUBLE_TYPE)) {
-//            return Opcodes.DSTORE;
-//        } else if (type.equals(Type.LONG_TYPE)) {
-//            return Opcodes.LSTORE;
-//        } else if (type.equals(Type.FLOAT_TYPE)) {
-//            return Opcodes.FSTORE;
-//        } else if (type.equals(Type.OBJECT)) {
-//            return Opcodes.ASTORE;
-//        } else {
-//            throw new IllegalStateException(
-//                String.format("Unknown type: %s", type)
-//            );
-//        }
     }
 }
