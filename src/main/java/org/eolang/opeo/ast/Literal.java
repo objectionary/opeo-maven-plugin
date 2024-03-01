b53413f201aa5a0de553aaaafb45f298617202c1
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-03-01 17:05:21 +0300

    feat(#174): remove problem with literals convertation for long values

--- src/main/java/org/eolang/opeo/ast/Literal.java
+++ src/main/java/org/eolang/opeo/ast/Literal.java
@@ -157,31 +157,7 @@ public final class Literal implements AstNode, Typed {
 
     @Override
     public Iterable<Directive> toXmir() {
-        //@checkstyle MethodBodyCommentsCheck (30 line)
-        // @todo #168:90min Remove ad-hoc solution related to long literals.
-        //  Currently, long literals are not supported by the XMIR format.
-        //  This is a temporary solution to avoid the problem with long literals.
-        //  Most probably we would need to fix jtcop to allow long values.
-        //  After that, we can remove this ad-hoc solution. We should leave
-        //  only new DirectivesData(this.lvalue) in the return statement.
-        final Iterable<Directive> result;
-        if (this.ltype.equals(Type.LONG_TYPE)) {
-            result = new Directives()
-                .add("o")
-                .attr("base", "long")
-                .attr("data", "bytes")
-                .set(
-                    Literal.bytesToHex(
-                        ByteBuffer
-                            .allocate(Long.BYTES)
-                            .putLong((long) this.lvalue)
-                            .array()
-                    )
-                ).up();
-        } else {
-            result = new DirectivesData(this.lvalue);
-        }
-        return result;
+        return new DirectivesData(this.lvalue);
     }
 
     @Override
