da64268ede289feef4afabf1057fb4da520324ae
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-05-28 10:31:22 +0300

    feat(#229): add two more puzzles for refactoring

--- src/main/java/org/eolang/opeo/ast/Constant.java
+++ src/main/java/org/eolang/opeo/ast/Constant.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2023 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.eolang.opeo.ast;
 
 import java.util.Collections;
@@ -10,26 +33,42 @@ import org.objectweb.asm.Type;
 import org.xembly.Directive;
 import org.xembly.Directives;
 
+/**
+ * Constant.
+ * This class represents LDC instruction in the JVM bytecode.
+ * @since 0.2
+ * @todo #229:90min Do we need {@link Constant} class?
+ *  It seems that this class is rather similar to {@link Literal} class.
+ *  We need to investigate if we can remove this class and use {@link Literal} instead.
+ *  If we can't remove this class, we need to add more tests to cover all the cases
+ *  where {@link Constant} is used.
+ */
 public final class Constant implements AstNode, Typed {
 
+    /**
+     * The constant value.
+     */
     private final Object value;
 
+    /**
+     * Constructor.
+     * @param node The XMIR node with value to parse.
+     */
     public Constant(final XmlNode node) {
-        this(parse(node));
+        this(Constant.parse(node));
     }
 
+    /**
+     * Constructor.
+     * @param value The constant value.
+     */
     public Constant(final Object value) {
         this.value = value;
     }
 
     @Override
     public List<AstNode> opcodes() {
-//        if (this.value instanceof Type) {
-//            return Collections.singletonList(
-//                new Opcode(Opcodes.LDC, ((Type) this.value).getClass()));
-//        } else {
-            return Collections.singletonList(new Opcode(Opcodes.LDC, this.value));
-//        }
+        return Collections.singletonList(new Opcode(Opcodes.LDC, this.value));
     }
 
     @Override
@@ -49,6 +88,11 @@ public final class Constant implements AstNode, Typed {
         }
     }
 
+    /**
+     * Parse the Constant value from XMIR representation.
+     * @param node The node to parse.
+     * @return The parsed value.
+     */
     private static Object parse(final XmlNode node) {
         final XmlNode child = node.firstChild();
         return DataType.find(child.attribute("base").orElseThrow())
--- src/main/java/org/eolang/opeo/ast/Duplicate.java
+++ src/main/java/org/eolang/opeo/ast/Duplicate.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2023 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.eolang.opeo.ast;
 
 import java.util.Collections;
@@ -11,29 +34,52 @@ import org.objectweb.asm.Type;
 import org.xembly.Directive;
 import org.xembly.Directives;
 
+/**
+ * Duplicate.
+ * This class represents the DUP instruction in the JVM bytecode.
+ * @since 0.2
+ * @todo #229:90min Do we need {@link Duplicate} class?
+ *  We have three classes with rather similar semantics: {@link Duplicate},
+ *  {@link Reference} and {@link Linked}.
+ *  They have similar methods and fields. We need to investigate if we can
+ *  remove one or two of these classes and use the remaining one instead.
+ */
 public final class Duplicate implements AstNode, Typed, Linked {
 
-    private final AtomicBoolean convertedToOpcodes;
-    private final AtomicBoolean convertedToXmir;
+    /**
+     * Flag to indicate if the node was compiled.
+     */
+    private final AtomicBoolean compiled;
 
+    /**
+     * Flag to indicate if the node was decompiled.
+     */
+    private final AtomicBoolean decompiled;
+
+    /**
+     * The original node which was duplicated.
+     */
     private final AtomicReference<AstNode> original;
 
+    /**
+     * Constructor.
+     * @param original The original node to duplicate.
+     */
     public Duplicate(final AstNode original) {
         this.original = new AtomicReference<>(original);
-        this.convertedToOpcodes = new AtomicBoolean(false);
-        this.convertedToXmir = new AtomicBoolean(false);
+        this.compiled = new AtomicBoolean(false);
+        this.decompiled = new AtomicBoolean(false);
     }
 
     @Override
     public List<AstNode> opcodes() {
-        if (this.convertedToOpcodes.getAndSet(true)) {
+        if (this.compiled.getAndSet(true)) {
             return Collections.emptyList();
         }
         return Stream.concat(
             this.original.get().opcodes().stream(),
             Stream.of(new Opcode(Opcodes.DUP))
         ).collect(Collectors.toList());
-//        return this.original.get().opcodes().stream().collect(Collectors.toList());
     }
 
     @Override
@@ -47,7 +93,7 @@ public final class Duplicate implements AstNode, Typed, Linked {
 
     @Override
     public Iterable<Directive> toXmir() {
-        if (this.convertedToXmir.getAndSet(true)) {
+        if (this.decompiled.getAndSet(true)) {
             return Collections.emptyList();
         }
         return new Directives()
@@ -57,10 +103,6 @@ public final class Duplicate implements AstNode, Typed, Linked {
             .up();
     }
 
-    public AstNode origin() {
-        return this.original.get();
-    }
-
     @Override
     public void link(final AstNode node) {
         this.original.set(node);
@@ -68,6 +110,14 @@ public final class Duplicate implements AstNode, Typed, Linked {
 
     @Override
     public AstNode current() {
+        return this.original.get();
+    }
+
+    /**
+     * Get the original node which was duplicated.
+     * @return The original node.
+     */
+    public AstNode origin() {
         return this.original.get();
     }
 }
