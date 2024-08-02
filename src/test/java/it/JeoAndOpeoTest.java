2b37e3beb42a41b868e6645cae76943975ca86f0
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-08-02 12:54:52 +0300

    feat(#344): identify one more problematic case

--- src/test/java/it/JeoAndOpeoTest.java
+++ src/test/java/it/JeoAndOpeoTest.java
@@ -127,7 +127,8 @@ final class JeoAndOpeoTest {
         "xmir/disassembled/Main.xmir",
         "xmir/disassembled/UndertowWebServerFactoryCustomizer$ServerOptions.xmir",
         "xmir/disassembled/MutableCoercionConfig.xmir",
-        "xmir/disassembled/WebProperties$Resources$Chain$Strategy$Content.xmir"
+        "xmir/disassembled/WebProperties$Resources$Chain$Strategy$Content.xmir",
+        "xmir/disassembled/OAuth2ClientRegistrationRepositoryConfiguration.xmir"
     })
     void decompilesCompilesAndKeepsTheSameInstructions(final String path) throws Exception {
         final XMLDocument original = new XMLDocument(new BytesOf(new ResourceOf(path)).asBytes());
