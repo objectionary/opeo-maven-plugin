0b58231304aecde132f32e4533b815a85d399241
rultor <me@rultor.com>
2024-08-06 11:24:34 +0000

    Deploying to gh-pages from @ objectionary/opeo-maven-plugin@a9a2083a9ea6dba52d56d4bd34a75c113143427c 🚀

---
+++ apidocs-0.3.1/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.eolang.opeo"},{"l":"org.eolang.opeo.ast"},{"l":"org.eolang.opeo.compilation"},{"l":"org.eolang.opeo.decompilation"},{"l":"org.eolang.opeo.decompilation.handlers"},{"l":"org.eolang.opeo.jeo"},{"l":"org.eolang.opeo.storage"}]
